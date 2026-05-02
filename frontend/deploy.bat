29745a3b711fba9248242b2f2a1b92314cf6edaa
ChareshReddy <chareshreddy@gmail.com>
2026-05-02 18:40:45 +0530

    Deployment Fix: Implemented clean-build logic to force cache refresh and ensure branding updates apply correctly

--- frontend/deploy.bat
+++ frontend/deploy.bat
@@ -1,3 +1,4 @@
+cmd.exe /c "rmdir /s /q "dist""
 npm run build
-cmd.exe /c "rmdir /s /q "..\MCA_Portable_App\frontend\dist\assets""
+cmd.exe /c "rmdir /s /q "..\MCA_Portable_App\frontend\dist""
 cmd.exe /c "xcopy /s /e /y dist "..\MCA_Portable_App\frontend\dist\""
